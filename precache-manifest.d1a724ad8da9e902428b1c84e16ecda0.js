6d2314d1e37436bf2779a55e6e1b8c959de218f8
True14 <jonnyfitzgib@gmail.com>
2019-09-27 12:09:22 -0400

    Updates

---
+++ precache-manifest.d1a724ad8da9e902428b1c84e16ecda0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268e4807925da794850e9ab68bfd1d47",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "fb63b55106b9af3450d0",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "fc9a4b52585ac6c9c081",
+    "url": "/robofriends/static/css/main.72a433cc.chunk.css"
+  },
+  {
+    "revision": "fb63b55106b9af3450d0",
+    "url": "/robofriends/static/js/2.c23c0b20.chunk.js"
+  },
+  {
+    "revision": "fc9a4b52585ac6c9c081",
+    "url": "/robofriends/static/js/main.8e7382a9.chunk.js"
+  },
+  {
+    "revision": "d7d3c3ad781ab5e09c65",
+    "url": "/robofriends/static/js/runtime-main.a9498424.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);

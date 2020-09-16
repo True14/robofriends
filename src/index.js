dfc42869c5a8585eb33c884db8cee5fe8cb75a33
True14 <jonnyfitzgib@gmail.com>
2020-09-16 15:28:28 -0400

    Register service worker

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ import * as serviceWorker from './serviceWorker';
 
 const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots })
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
